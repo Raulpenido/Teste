fdd76c824458faa73ab0499314bf1f5c002aeaa6
Raulpenido <raul.goncalvepenido@gmail.com>
2025-08-13 16:27:48 -0300

    Codigo verificador Par ou Impar.c

--- Par ou Impar.c
+++ Par ou Impar.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+  int numero;
+  scanf("%d", &numero);
   printf("Hello world:");
+  if(numero % 2 == 0){
+    printf(" 1 ");
+  }else{ 
+    printf(" 0 ");
+  }
+  
 return 0;
 }
